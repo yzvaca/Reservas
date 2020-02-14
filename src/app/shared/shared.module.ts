99f5d4508d86d2d53a17661dbb698c1ef1c000a0
Yazidy Zuryndernath Vaca Barba <yvaca@emacruz.com.bo>
2020-02-14 10:24:22 -0400

    started map development

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+
+import { LocationPickerComponent } from './pickers/location-picker/location-picker.component';
+import { MapModalComponent } from './map-modal/map-modal.component';
+
+@NgModule({
+    declarations: [LocationPickerComponent, MapModalComponent],
+    imports: [CommonModule, IonicModule],
+    exports: [LocationPickerComponent, MapModalComponent],
+    entryComponents: [MapModalComponent]
+})
+export class SharedModule { }
